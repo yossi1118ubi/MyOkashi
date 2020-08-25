f0a50b1aff4777da341ad540041af5d6d1e05042
Daichi Yoshikawa <yossi@ubi.cs.ritsumei.ac.jp>
2020-08-25 11:29:59 +0900

    完成

--- MyOkashi/ViewController.swift
+++ MyOkashi/ViewController.swift
@@ -7,14 +7,175 @@
 //
 
 import UIKit
+import SafariServices
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UISearchBarDelegate, UITableViewDataSource, UITableViewDelegate, SFSafariViewControllerDelegate{
+    
+    //viewDidLoad()は初めて画面が表示されるときに実行される
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //SearchBarのdelegate通知先を設定
+        searchText.delegate = self
+        
+        //入力のヒントとなるプレースホルダーを設定
+        searchText.placeholder = "お菓子の名前を入力してください"
+        
+        //Table ViewのdataSourceを設定
+        tableView.dataSource = self
+        
+        //Table Viewのdelegateの設定
+        tableView.delegate = self
     }
 
-
+    @IBOutlet weak var searchText: UISearchBar!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var okashiList: [(name: String, maker: String, link: URL, image: URL)] = []
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar){
+        //キーボードを閉じる
+        view.endEditing(true)
+        
+        if let searchWord = searchBar.text{
+            //デバックエリアに出力
+            print(searchWord)
+            
+            //入力されていたら, お菓子を検索
+            searchOkashi(keyword: searchWord)
+        }
+    }
+    
+    //JSONのitem内のデータ構造
+    struct ItemJson: Codable{
+        //お菓子の名称
+        let name: String?
+        //メーカー
+        let maker: String?
+        
+        //記載URL
+        let url: URL?
+        
+        //画像URL
+        let image: URL?
+        
+    }
+    
+    //JSONのデータ構造
+    struct ResultJson: Codable{
+        //複数要素
+        let item:[ItemJson]?
+    }
+    
+    //searchOkashiメソッド
+    //第一引数: keyword 検索したいワード
+    func searchOkashi(keyword: String){
+        //お菓子の検索ワードをURLエンコードする
+        guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else{
+            return
+        }
+        
+        //リクエストURLの組み立て
+        guard let req_url = URL(string: "https://sysbird.jp/toriko/api/?apikey=guest&format=json&keyword=\(keyword_encode)&max=10&order=r")else {
+            return
+        }
+        print(req_url)
+        
+        //リクエストに必要な情報を生成
+        let req = URLRequest(url: req_url)
+        //データ転送を管理するためのセッションを生成
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        //リクエストをタスクとして登録
+        let task = session.dataTask(with: req, completionHandler: {
+            (data, response, error) in
+            session.finishTasksAndInvalidate()
+            //do try catch エラーハンドリング
+            do{
+                //JsonDecodeのインスタンス取得
+                let decoder = JSONDecoder()
+                //受け取ったJSONデータをパース(解析)して格納
+                let json = try decoder.decode(ResultJson.self, from: data!)
+                
+                //お菓子の情報が取得できているか確認
+                if let items = json.item{
+                    //お菓子のリストを初期化
+                    self.okashiList.removeAll()
+                    //取得しているお菓子の数だけ処理
+                    for item in items{
+                        //お菓子の名称, メーカー名, 記載URL, 画像URLをアンラップ
+                        if let name = item.name, let maker = item.maker, let link = item.url, let image = item.image{
+                            //1つのお菓子をタプルでまとめて管理
+                            let okashi = (name, maker, link, image)
+                            //お菓子の配列へ追加
+                            self.okashiList.append(okashi)
+                            
+                        }
+                        
+                    }
+                    //Table Viewを更新する
+                    self.tableView.reloadData()
+                    if let okashidb = self.okashiList.first{
+                        print("--------------")
+                        print("okashiList[0] = \(okashidb)")
+                    }
+                    
+                }
+                
+            }catch{
+                //エラー処理
+                print("エラーが出ました")
+            }
+        })
+        
+        //ダウンロード開始
+        task.resume()
+    }
+    
+    //Cellの総数を返すdatasourceメソッド, 必ず記述する必要がある
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        //お菓子のリストの総数
+        return okashiList.count
+    }
+    
+    //Cellに値を設定するdatasourceメソッド, 必ず記載する必要がある
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //今回表示を行う, cellオブジェクト(1行)を取得する
+        let cell = tableView.dequeueReusableCell(withIdentifier: "okashiCell", for: indexPath)
+        //お菓子のタイトルを設定
+        cell.textLabel?.text = okashiList[indexPath.row].name
+        //お菓子画像を取得
+        if let imageData = try? Data(contentsOf: okashiList[indexPath.row].image){
+            //正常に取得できた場合は, UIImageで画像オブジェクトを生成して, Cellにお菓子画像を」設定
+            cell.imageView?.image = UIImage(data: imageData)
+            
+        }
+        //設定済みのCellオブジェクトを画面に反映
+        return cell
+    }
+    
+    //Cellが選択された際に呼び出されるdelegateメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        //ハイライト解除
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        //SFsafariViewを開く
+        let safariViewController = SFSafariViewController(url: okashiList[indexPath.row].link)
+        
+        //delegateの通知先を自分自身
+        safariViewController.delegate = self
+        
+        //SafariViewが開かれる
+        present(safariViewController, animated: true, completion: nil)
+    }
+    
+    //SafariViewが閉じられた時に呼ばれるdelegateメソッド
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController){
+        //SafariViewを閉じる
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
 
