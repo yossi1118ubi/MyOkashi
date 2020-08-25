298cb5471fbfd850fb2ef22fa8e1567abacb6dc9
Daichi Yoshikawa <yossi@ubi.cs.ritsumei.ac.jp>
2020-08-25 09:40:11 +0900

    ファーストコミット

---
+++ MyOkashi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyOkashi
+//
+//  Created by Daichi Yoshikawa on 2020/08/25.
+//  Copyright © 2020 Daichi Yoshikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
